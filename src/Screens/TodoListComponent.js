43062385de2c433be7829984ba5a14654b94f7ec
Gurpreet Singhgit config --global user.email gurpreetsingh2808@gmail.com <gurpreetsingh2808@gmail.com>
2018-02-26 00:26:59 +0530

    Created swipeable list using dummy data

---
+++ src/Screens/TodoListComponent.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { StyleSheet, Text, View, ListView} from 'react-native';
+import {Container, Button, Icon, Content, Header, List, ListItem} from 'native-base';
+
+const datas = [
+    'Simon Mignolet',
+    'Nathaniel Clyne',
+    'Dejan Lovren',
+    'Mama Sakho',
+    'Alberto Moreno',
+    'Emre Can',
+    'Joe Allen',
+    'Phil Coutinho',
+];
+
+export default class TodoListComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.state = {
+            basic: true,
+            listViewData: datas,
+        };
+    }
+    deleteRow(secId, rowId, rowMap) {
+        rowMap[`${secId}${rowId}`].props.closeRow();
+        const newData = [...this.state.listViewData];
+        newData.splice(rowId, 1);
+        this.setState({ listViewData: newData });
+    }
+    render() {
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        return (
+            <Container>
+                <Header />
+                <Content>
+                    <List
+                        dataSource={this.ds.cloneWithRows(this.state.listViewData)}
+                        renderRow={data =>
+                            <ListItem>
+                                <Text> {data} </Text>
+                            </ListItem>}
+                        renderLeftHiddenRow={data =>
+                            <Button full onPress={() => alert(data)}>
+                                <Icon active name="information-circle" />
+                            </Button>}
+                        renderRightHiddenRow={(data, secId, rowId, rowMap) =>
+                            <Button full danger onPress={_ => this.deleteRow(secId, rowId, rowMap)}>
+                                <Icon active name="trash" />
+                            </Button>}
+                        leftOpenValue={75}
+                        rightOpenValue={-75}
+                    />
+                </Content>
+            </Container>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
